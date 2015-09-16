5b7948716cc29a33ab82ef10f19c64835eefe65a
meta-priyamvada-chundawat <priyamvada.chundawat@metacube.com>
2015-09-16 17:44:32 +0530

    Create TitleAuthor.java

---
+++ JDBC/TitleAuthor.java
@@ -0,0 +1,25 @@
+// POJO class of TitleAuthor
+public class TitleAuthor {
+	private int titleIdofTitleAuthor;
+	private int authorIdOfTitleAuthor;
+
+	// return titleIdofTitleAuthor
+	public int getTitleIdofTitleAuthor() {
+		return titleIdofTitleAuthor;
+	}
+
+	// set titleIdofTitleAuthor
+	public void setTitleIdofTitleAuthor(int titleIdofTitleAuthor) {
+		this.titleIdofTitleAuthor = titleIdofTitleAuthor;
+	}
+
+	// return authorIdOfTitleAuthor
+	public int getAuthorIdOfTitleAuthor() {
+		return authorIdOfTitleAuthor;
+	}
+
+	// set authorIdOfTitleAuthor
+	public void setAuthorIdOfTitleAuthor(int authorIdOfTitleAuthor) {
+		this.authorIdOfTitleAuthor = authorIdOfTitleAuthor;
+	}
+}
