334b610d0b7f1caf735873055a60375667c5760b
meta-priyamvada-chundawat <priyamvada.chundawat@metacube.com>
2015-09-16 17:41:39 +0530

    Create MenuClass.java

---
+++ JDBC/MenuClass.java
@@ -0,0 +1,10 @@
+// Class to print menu
+public class MenuClass {
+	public void menuForLibraryManagement() {
+		System.out.println("Menu :");
+		System.out.println("1.To display books published by given author");
+		System.out.println("2.Add a new member and issue the given book");
+		System.out
+				.println("3.Delete all those books which were not issued in last 1 year");
+	}
+}
