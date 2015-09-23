925720be86bb86a60cd3158963b85254b1e2043b
meta-priyamvada-chundawat <priyamvada.chundawat@metacube.com>
2015-09-23 10:42:16 +0530

    Create VehiclePrinter.java

---
+++ CaseStudy2/VehiclePrinter.java
@@ -0,0 +1,13 @@
+public class VehiclePrinter {
+	/**
+	 * 
+	 * Function to print vehicles
+	 * 
+	 * @param vehicle
+	 * @return
+	 */
+	public static String printVehicleSpecification(Vehicle vehicle) {
+
+		return vehicle.toString();
+  }
+}
